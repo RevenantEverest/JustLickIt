0a83dfc615cbef486a50d33fbdac6e736312533b
Stefan <stefischer@mail.com>
2019-02-16 20:25:19 -0500

    Component/File structure setup

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './HomePage.css';
+
+class HomePage extends Component {
+
+    render() {
+        return(
+            <div id="HomePage">
+            </div>
+        );
+    }
+};
+
+export default HomePage;
